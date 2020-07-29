08669fc9c9ab948a45fd648fc0e3d11f997e7f1d
Davidoffili <palagashvilid@gmail.com>
2020-07-29 16:09:41 -0700

    modified file structure, starter header component

---
+++ src/components/layout/header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { Paper, Container, Typography } from '@material-ui/core';
+
+import { ShoppingCartOutlined } from '@material-ui/icons';
+
+import { makeStyles } from '@material-ui/core/styles';
+
